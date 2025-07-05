2732f0b5afa7bf91bb2398922d1f7fb0a91753ae
tjdms4327 <tjdms4327@gmail.com>
2025-07-06 02:08:29 +0900

    update comment

--- 백준/Silver/4158. CD/CD.py
+++ 백준/Silver/4158. CD/CD.py
@@ -15,7 +15,7 @@ def count_common(N, A, M, B): # 한 테스트케이스에서 common element 수 
             j+=1
     return count
 
-while True: # 여러개의 테스트 케이스
+while True: # 여러 개의 테스트 케이스
     N, M= map(int, input().strip().split())
     if N==0 and M==0: break # 입력 마지막 줄은 0 0
         
