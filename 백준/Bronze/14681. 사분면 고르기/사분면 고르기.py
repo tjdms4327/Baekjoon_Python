0208ec5f8f645231760b58ec8abc3073f271340b
tjdms4327 <tjdms4327@gmail.com>
2025-07-06 22:16:55 +0900

    [Bronze V] Title: 사분면 고르기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,19 @@
+x=int(input())
+y=int(input())
+
+if x>0:
+    if y>0:
+        print(1)
+    elif y<0:
+        print(4)
+    else:
+        pass
+elif x<0:
+    if y>0:
+        print(2)
+    elif y<0:
+        print(3)
+    else:
+        pass
+else:
+    pass
