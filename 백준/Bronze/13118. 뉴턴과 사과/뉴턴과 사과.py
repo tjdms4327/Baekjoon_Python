cfb32411863f224aaa37c918f7c5d5795d1cbceb
tjdms4327 <tjdms4327@gmail.com>
2025-07-06 22:09:29 +0900

    [Bronze IV] Title: 뉴턴과 사과, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/13118. 뉴턴과 사과/뉴턴과 사과.py
@@ -0,0 +1,14 @@
+a,b,c,d=map(int,input().split())
+x,y,r=map(int,input().split())
+
+if a==x:
+    print(1)
+elif b==x:
+    print(2)
+elif c==x:
+    print(3)
+elif d==x:
+    print(4)
+else:
+    print(0)
+    
