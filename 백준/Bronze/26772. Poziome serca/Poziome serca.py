661ac64df1745afbf7a1954650173a74f1a59d0a
tjdms4327 <tjdms4327@gmail.com>
2025-07-07 18:14:39 +0900

    [Bronze IV] Title: Poziome serca, Time: 52 ms, Memory: 33460 KB -BaekjoonHub

---
+++ 백준/Bronze/26772. Poziome serca/Poziome serca.py
@@ -0,0 +1,15 @@
+def heart(n):
+    print(' @@@   @@@  '*n)
+    print('@   @ @   @ '*n)
+    print('@    @    @ '*n)
+    print('@         @ '*n)
+    print(' @       @  '*n)
+    print('  @     @   '*n)
+    print('   @   @    '*n)
+    print('    @ @     '*n)
+    print('     @      '*n)
+    
+    
+n=int(input())
+
+heart(n)
