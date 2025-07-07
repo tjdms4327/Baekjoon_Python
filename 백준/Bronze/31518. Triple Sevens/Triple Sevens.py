43dd7cd0aad76b6c53e639c0c7d8caa5fdba7f8d
tjdms4327 <tjdms4327@gmail.com>
2025-07-07 18:45:45 +0900

    [Bronze IV] Title: Triple Sevens, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/31518. Triple Sevens/Triple Sevens.py
@@ -0,0 +1,9 @@
+n=int(input())
+a=list(map(int, input().split()))
+b=list(map(int, input().split()))
+c=list(map(int, input().split()))
+
+if (7 in a) and (7 in b) and (7 in c):
+    print(777)
+else:
+    print(0)
