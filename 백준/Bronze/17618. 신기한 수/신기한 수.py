41d0590905d9f2646984932949f7f1a75ee69aff
tjdms4327 <tjdms4327@gmail.com>
2025-07-13 01:54:05 +0900

    [Bronze III] Title: 신기한 수, Time: 712 ms, Memory: 110708 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17618. 신기한 수/신기한 수.py
+++ 백준/Bronze/17618. 신기한 수/신기한 수.py
@@ -1,11 +1,17 @@
 import sys
 input=sys.stdin.readline
+
+def sum_digit(i):
+    s=0
+    while i:
+        s+=(i%10)
+        i//=10
+    return s
     
 n=int(input().strip())
 cnt=0
 for i in range(1, n+1):
     if i<10: cnt+=1
     else:
-        i_digit=list(map(int, str(i)))
-        if i%sum(i_digit)==0: cnt+=1
+        if i%sum_digit(i)==0: cnt+=1
 print(cnt)
