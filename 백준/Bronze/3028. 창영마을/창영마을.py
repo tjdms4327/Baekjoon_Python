57db206108e20cd4b84400d21337bdad59cbe504
tjdms4327 <tjdms4327@gmail.com>
2025-07-23 17:15:25 +0900

    [Bronze II] Title: 창영마을, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/3028. 창영마을/창영마을.py
@@ -0,0 +1,11 @@
+s=input()
+
+ball=[1, 0, 0]
+for i in s:
+    if i=='A':
+        ball[0], ball[1] = ball[1], ball[0]
+    elif i=='B': 
+        ball[1], ball[2] = ball[2], ball[1]
+    else:
+        ball[0], ball[2] = ball[2], ball[0]
+print(ball.index(1)+1)
