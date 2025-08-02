f96145c30cbc0c627f6e137ee69b50c02a936375
tjdms4327 <tjdms4327@gmail.com>
2025-08-03 00:56:27 +0900

    [Gold V] Title: 내려가기, Time: 220 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -0,0 +1,28 @@
+import sys
+input=sys.stdin.readline
+
+n = int(input())
+a, b, c = map(int, input().split())
+
+
+
+max_dp, min_dp=[a, b, c], [a, b, c]
+
+for _ in range(n - 1):
+    a, b, c = map(int, input().split())
+    
+    new_max = [
+        max(max_dp[0], max_dp[1]) + a,
+        max(max_dp[0], max_dp[1], max_dp[2]) + b,
+        max(max_dp[1], max_dp[2]) + c
+    ]
+    max_dp = new_max
+    
+    new_min = [
+        min(min_dp[0], min_dp[1]) + a,
+        min(min_dp[0], min_dp[1], min_dp[2]) + b,
+        min(min_dp[1], min_dp[2]) + c
+    ]
+    min_dp = new_min
+
+print(max(max_dp), min(min_dp))
