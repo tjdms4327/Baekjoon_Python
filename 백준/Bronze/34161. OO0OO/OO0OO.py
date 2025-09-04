9aae07789a5adee01d16c2c5353392d696c3548c
tjdms4327 <tjdms4327@gmail.com>
2025-09-04 20:15:38 +0900

    [Bronze IV] Title: OO0OO, Time: 36 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/34161. OO0OO/OO0OO.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(3):
+    input()
+
+for A in range(1, 11):
+    for B in range(1, 11):
+        for C in range(1, 11):
+            for D in range(1, 11):
+                print(-1)
