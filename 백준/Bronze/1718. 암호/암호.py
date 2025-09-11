b6428ab867cf86a06e1f5410875151bcea15f987
tjdms4327 <tjdms4327@gmail.com>
2025-09-11 12:42:06 +0900

    [Bronze II] Title: 암호, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1718. 암호/암호.py
@@ -0,0 +1,7 @@
+s = list(input())
+key = list(input())
+
+for i in range(len(s)):
+    if 97 <= ord(s[i]) <= 122:
+        s[i] = chr(((ord(s[i]) - ord(key[i%len(key)]) -1) % 26) + 97)
+print("".join(s))
