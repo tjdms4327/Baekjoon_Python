3e30cd272ca17db854c81842f93bfb60255da4f8
tjdms4327 <tjdms4327@gmail.com>
2025-09-18 03:42:26 +0900

    [Bronze III] Title: FizzBuzz, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/14761. FizzBuzz/FizzBuzz.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+x, y, n = map(int, input().split())
+for i in range(1, n+1):
+    if i % x == 0:
+        if i % y == 0:
+            print('FizzBuzz')
+        else:
+            print('Fizz')
+    elif i % y == 0:
+        print('Buzz')
+    else:
+        print(i)
