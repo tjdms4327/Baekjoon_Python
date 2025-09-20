e871593e93b9da33773ae55056e3b3ecf53f56e4
tjdms4327 <tjdms4327@gmail.com>
2025-09-20 22:40:06 +0900

    [Silver III] Title: 소수 구하기, Time: 488 ms, Memory: 73956 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,14 +1,16 @@
-def prime(x):
-    if x<2:
-        return False
-    for i in range(2, int(x**0.5)+1):
-        if x%i==0:
-            return False
-    return True
-            
+import math
 
-a,b=map(int,input().split())
-while a<=b:
-    if prime(a):
-        print(a)
-    a+=1
+m, n = map(int, input().split())
+A = [0] * (n + 1)
+for i in range(2, n + 1):
+    A[i] = i
+    
+for i in range(2, int(math.sqrt(n)) + 1):
+    if A[i] == 0:
+        continue
+    for j in range(i+i, n+1, i):
+        A[j] = 0
+
+for i in range(m, n+1):
+    if A[i] != 0:
+        print(A[i])
