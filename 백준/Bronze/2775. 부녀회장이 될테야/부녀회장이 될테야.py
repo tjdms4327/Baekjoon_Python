3cbcdf4a2a3ec0cc311dfc4e6788b09249852165
tjdms4327 <tjdms4327@gmail.com>
2025-10-03 23:50:09 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,11 +1,18 @@
-def come_with(k,n):
-    people=[i for i in range(1, n+1)]
-    for _ in range(k):
-        people=[sum(people[0:i]) for i in range(1, n+1)]
-    return people[n-1]
-    
-t=int(input())
-for i in range(t):
-    k=int(input())
-    n=int(input())
-    print(come_with(k,n))
+# bronzeI_2775
+import sys
+input = sys.stdin.readline
+
+D = [[0 for j in range(15)] for i in range(15)]
+
+for i in range(1, 15):
+    D[0][i] = i
+    D[i][1] = 1
+for i in range(1, 15):
+    for j in range(2, 15):
+        D[i][j] = D[i][j-1] + D[i-1][j]
+
+t = int(input())
+for i in range(0, t):
+    k = int(input())
+    n = int(input())
+    print(D[k][n])
