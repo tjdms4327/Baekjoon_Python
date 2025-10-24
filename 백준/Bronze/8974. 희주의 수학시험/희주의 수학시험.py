4db47ad8470bcccfd178050ec83d275d14c0f990
tjdms4327 <tjdms4327@gmail.com>
2025-10-24 22:27:14 +0900

    [Bronze I] Title: 희주의 수학시험, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/8974. 희주의 수학시험/희주의 수학시험.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+nums = [0]
+for i in range(1, 45+1):
+    nums.extend([i]*i)
+    
+a, b = map(int, input().split())
+
+prefix_sum = [0] * (b+1)
+for i in range(1, b+1):
+    prefix_sum[i] = prefix_sum[i-1] + nums[i]
+print(prefix_sum[b] - prefix_sum[a-1])
