eb3c0844eed1acda7a829bd4b5fc14f05f1e9e56
tjdms4327 <tjdms4327@gmail.com>
2025-10-28 14:32:43 +0900

    [Bronze I] Title: 열 순서, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/16495. 열 순서/열 순서.py
@@ -0,0 +1,16 @@
+# bronzeI_16495
+import sys
+input = sys.stdin.readline
+
+s = input().strip()
+n = len(s) - 1
+
+col_num = 0
+for ch in s:
+    cur = ord(ch) - ord('A') + 1
+    if n > 0:
+        col_num += 26**n * cur
+    else:
+        col_num += cur
+    n -= 1
+print(col_num)
