f9362518d4860baf0c6f5d7ecfd37e5d7ac4deb4
tjdms4327 <tjdms4327@gmail.com>
2025-11-06 22:34:06 +0900

    [Bronze II] Title: 악마의 제안, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/23972. 악마의 제안/악마의 제안.py
@@ -0,0 +1,12 @@
+# bronzeII_23972
+import sys
+input = sys.stdin.readline
+
+k, n = map(int, input().split())
+
+if(n == 1): 
+    print(-1)
+else:
+    ans = (k*n)//(n-1)
+    if((k*n) % (n-1)): ans += 1 
+    print(ans)
