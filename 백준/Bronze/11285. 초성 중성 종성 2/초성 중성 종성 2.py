21605a17138f8252a27d68e033ee371c93da7c20
tjdms4327 <tjdms4327@gmail.com>
2025-11-14 20:26:41 +0900

    [Bronze I] Title: 초성 중성 종성 2, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/11285. 초성 중성 종성 2/초성 중성 종성 2.py
@@ -0,0 +1,18 @@
+# bronzeI_11285
+import sys
+
+CHOSUNG_LIST = ['ㄱ','ㄲ','ㄴ','ㄷ','ㄸ','ㄹ','ㅁ','ㅂ','ㅃ','ㅅ','ㅆ','ㅇ','ㅈ','ㅉ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ']
+JUNGSUNG_LIST = ['ㅏ','ㅐ','ㅑ','ㅒ','ㅓ','ㅔ','ㅕ','ㅖ','ㅗ','ㅘ','ㅙ','ㅚ','ㅛ','ㅜ','ㅝ','ㅞ','ㅟ','ㅠ','ㅡ','ㅢ','ㅣ']
+JONGSUNG_LIST = ['','ㄱ','ㄲ','ㄳ','ㄴ','ㄵ','ㄶ','ㄷ','ㄹ','ㄺ','ㄻ','ㄼ','ㄽ','ㄾ','ㄿ','ㅀ','ㅁ','ㅂ','ㅄ','ㅅ','ㅆ','ㅇ','ㅈ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ']
+
+lst = []
+for line in sys.stdin.read().splitlines():
+    if not line:
+        continue
+    lst.append(line)
+
+cho = CHOSUNG_LIST.index(lst[0])
+jung = JUNGSUNG_LIST.index(lst[1])
+jong = JONGSUNG_LIST.index(lst[2]) if len(lst) == 3 else 0
+code = 0xAC00 + (cho * 21 + jung) * 28 + jong
+print(chr(code))
