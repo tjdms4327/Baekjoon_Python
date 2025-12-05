9199f7f566d6eaa3876d0764f8037d19992d949d
tjdms4327 <tjdms4327@gmail.com>
2025-12-05 12:21:12 +0900

    [Bronze III] Title: Increasing Sublist, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30216. Increasing Sublist/Increasing Sublist.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+A = list(map(int, input().split()))
+
+temp, max_len = 1, 0
+for i in range(1,n):
+    if A[i-1] < A[i]:
+        temp += 1
+    else:
+        max_len = max(max_len, temp)
+        temp = 1
+max_len = max(max_len, temp)
+print(max_len)
