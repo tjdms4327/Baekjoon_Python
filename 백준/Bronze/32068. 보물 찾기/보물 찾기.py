9a80c09014012784d21b2024c80f9504f778470f
tjdms4327 <tjdms4327@gmail.com>
2025-12-26 20:28:18 +0900

    [Bronze II] Title: 보물 찾기, Time: 56 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/32068. 보물 찾기/보물 찾기.py
+++ 백준/Bronze/32068. 보물 찾기/보물 찾기.py
@@ -5,13 +5,6 @@ t = int(input())
 for _ in range(t):
     l, r, s = map(int, input().split())
     
-    count = 1
-    i = 1
-    while s!=l and s!=r:
-        if i%2==1:
-            s += i
-        else:
-            s -= i
-        i += 1
-        count += 1
-    print(count)
+    right = 2*(r-s)
+    left = 2*(s-l) + 1
+    print(min(right, left))
