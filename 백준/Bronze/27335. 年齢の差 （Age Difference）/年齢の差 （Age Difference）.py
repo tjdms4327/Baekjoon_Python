5decab4666ba5fe9a15f6fe38fc7eb8de0c4c420
tjdms4327 <tjdms4327@gmail.com>
2026-02-01 14:47:52 +0900

    [Bronze II] Title: 年齢の差 (Age Difference), Time: 236 ms, Memory: 153040 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27335. 年齢の差 （Age Difference）/年齢の差 （Age Difference）.py
+++ 백준/Bronze/27335. 年齢の差 （Age Difference）/年齢の差 （Age Difference）.py
@@ -4,10 +4,32 @@ input = sys.stdin.readline
 n = int(input())
 A = list(map(int, input().split()))
 
+
+lmin = [0]*n
+lmax = [0]*n
+rmin = [0]*n
+rmax = [0]*n
+
+lmin[0] = lmax[0] = A[0]
+for i in range(1, n):
+    lmin[i] = min(lmin[i-1], A[i])
+    lmax[i] = max(lmax[i-1], A[i])
+
+rmin[-1] = rmax[-1] = A[-1]
+for i in range(n-2, -1, -1):
+    rmin[i] = min(rmin[i+1], A[i])
+    rmax[i] = max(rmax[i+1], A[i])
+
+
 for i in range(n):
-    best = 0
-    for j in range(n):
-        if i != j:
-            x = abs(A[i] - A[j])
-            best = max(best, x)
-    print(best)
+    if i == 0:
+        other_min = rmin[1]
+        other_max = rmax[1]
+    elif i == n-1:
+        other_min = lmin[n-2]
+        other_max = lmax[n-2]
+    else:
+        other_min = min(lmin[i-1], rmin[i+1])
+        other_max = max(lmax[i-1], rmax[i+1])
+
+    print(max(abs(A[i] - other_min), abs(A[i] - other_max)))
