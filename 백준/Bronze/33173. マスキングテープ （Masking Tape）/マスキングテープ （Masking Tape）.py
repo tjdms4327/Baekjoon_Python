07baf7cfafaefd3bffb5ea7af8c1c1d4674ab9df
tjdms4327 <tjdms4327@gmail.com>
2026-02-01 15:40:35 +0900

    [Bronze II] Title: マスキングテープ (Masking Tape), Time: 336 ms, Memory: 136728 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/33173. マスキングテープ （Masking Tape）/マスキングテープ （Masking Tape）.py
+++ 백준/Bronze/33173. マスキングテープ （Masking Tape）/マスキングテープ （Masking Tape）.py
@@ -2,25 +2,27 @@ import sys
 input = sys.stdin.readline
 
 h, w, q = map(int, input().split())
+queries = [list(map(int, input().split())) for _ in range(q)]
 
-blocks = [[False]*w for _ in range(h)]
-color = [[0]*w for _ in range(h)]
+ans = [[0]*w for _ in range(h)]
+blocked = [[False]*w for _ in range(h)]
 
-for _ in range(q):
-    line = input().strip().split()
-    if line[0]=='1':
-        x, y, c = map(int, line[1:])
-        x = int(x) ; y = int(y)
-        for col in range(max(0, y-1), min(h, y+1)):
-            for row in range(max(0, x-1), min(h, x+1)):
-                if blocks[row][col] != True:
-                    color[row][col] = int(c)
+for q in queries:
+    if q[0] == 1:
+        _, x, y, c = q
+        x -= 1
+        y -= 1
+        for i in range(x, x+2):
+            for j in range(y, y+2):
+                if not blocked[i][j]:
+                    ans[i][j] = c
     else:
-        x, y = map(int, line[1:])
-        x = int(x) ; y = int(y)
-        for col in range(max(0, y-1), min(h, y+1)):
-            for row in range(max(0, x-1), min(h, x+1)):
-                blocks[row][col] = True
-    
-for row in color:
-    print(*row)
+        _, x, y = q
+        x -= 1
+        y -= 1
+        for i in range(x, x+2):
+            for j in range(y, y+2):
+                blocked[i][j] = True
+
+for i in range(h):
+    print(*ans[i])
