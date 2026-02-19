19a88096e1fcab2be57ec3edd6a5ea71134e6723
tjdms4327 <tjdms4327@gmail.com>
2026-02-19 22:16:57 +0900

    [Silver II] Title: 최대 힙, Time: 160 ms, Memory: 114204 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+import heapq
+
+n = int(input())
+
+heap = []
+for _ in range(n):
+    x = int(input())
+    
+    if x == 0:
+        if heap:
+            print(-heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, -x)
