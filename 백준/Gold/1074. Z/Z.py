811c8191fd0b5aa14aac373f716f95a3b3c4d719
tjdms4327 <tjdms4327@gmail.com>
2026-02-19 18:05:38 +0900

    [Gold V] Title: Z, Time: 80 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n, r, c = map(int, input().split())
+
+def solve(n, r, c):
+    if n == 0:
+        return 0
+    
+    half = 2 ** (n-1)
+    area = half * half
+    
+    if r < half and c < half:
+        return solve(n-1, r, c)
+    
+    elif r < half and c >= half:
+        return area + solve(n-1, r, c-half)
+    
+    elif r >= half and c < half:
+        return 2*area + solve(n-1, r-half, c)
+    
+    else:
+        return 3*area + solve(n-1, r-half, c-half)
+
+print(solve(n, r, c))
