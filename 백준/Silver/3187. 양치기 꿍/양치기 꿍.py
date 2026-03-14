9a209fe83858a2b998dc700343511b4b09cc33d1
tjdms4327 <tjdms4327@gmail.com>
2026-03-14 21:07:59 +0900

    [Silver I] Title: 양치기 꿍, Time: 160 ms, Memory: 117760 KB -BaekjoonHub

---
+++ 백준/Silver/3187. 양치기 꿍/양치기 꿍.py
@@ -0,0 +1,45 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+r, c = map(int, input().split())
+yard = [list(input().strip()) for _ in range(r)]
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+total_sheep, total_wolf = 0, 0
+
+visited = [[False]*c for _ in range(r)]
+for x in range(r):
+    for y in range(c):
+        if not visited[x][y] and yard[x][y]!='#':
+            queue = deque([(x, y)])
+            visited[x][y] = True
+            
+            sheep = 0
+            wolf = 0
+            
+            while queue:
+                cx, cy = queue.popleft()    
+                if yard[cx][cy] == 'k':
+                    sheep += 1
+                elif yard[cx][cy] == 'v':
+                    wolf += 1
+                    
+                for i in range(4):
+                    nx = cx + dx[i]
+                    ny = cy + dy[i]
+                    
+                    if 0 <= nx < r and 0 <= ny < c:
+                        if not visited[nx][ny] and yard[nx][ny] != '#':
+                            visited[nx][ny] = True
+                            queue.append((nx, ny))
+                            
+            if sheep > wolf:
+                total_sheep += sheep
+            else:
+                total_wolf += wolf
+                
+                
+print(total_sheep, total_wolf)
