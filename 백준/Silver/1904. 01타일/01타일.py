50df6c90f6ecb23ffca0e185d18d66cbde5238a4
tjdms4327 <tjdms4327@gmail.com>
2026-04-06 14:39:38 +0900

    [Silver III] Title: 01타일, Time: 104 ms, Memory: 117360 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+if n == 1:
+    print(1)
+    sys.exit()
+
+dp = [0]*(n+1)
+dp[1] = 1
+dp[2] = 2
+for i in range(3, n+1):
+    dp[i] = (dp[i-2] + dp[i-1]) % 15746
+    
+print(dp[n])
