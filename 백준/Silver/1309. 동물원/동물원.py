090b9469d578fadea291ffc68b5dd851e3addb0c
tjdms4327 <tjdms4327@gmail.com>
2026-04-06 14:59:13 +0900

    [Silver I] Title: 동물원, Time: 108 ms, Memory: 116640 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [[0]*3 for _ in range(n+1)] # 줄에X, 왼쪽에 사자, 오른쪽에 사자
+dp[1] = [1,1,1]
+
+for i in range(2, n+1):
+    dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % 9901
+    dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % 9901
+    dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % 9901
+    
+print(sum(dp[n]) % 9901)
