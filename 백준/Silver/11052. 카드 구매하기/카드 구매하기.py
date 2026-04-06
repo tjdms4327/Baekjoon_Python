f251cf2f09bf26228162eeec0155b29db72784b3
tjdms4327 <tjdms4327@gmail.com>
2026-04-06 14:46:53 +0900

    [Silver I] Title: 카드 구매하기, Time: 104 ms, Memory: 110760 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+P = [0] + list(map(int, input().split()))
+
+dp = [0]*(n+1)
+dp[1] = P[1]
+
+for i in range(2, n+1):
+    dp[i] = max(dp[i-x] +P[x] for x in range(1, i+1))
+    
+print(dp[n])
